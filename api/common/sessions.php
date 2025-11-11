d21d7b17ae0059d1d893b86d32a1992ed0b2454c
snchzdss <221012763+snchzdss@users.noreply.github.com>
2025-11-11 17:06:14 +0800

    initial commit

---
+++ api/common/sessions.php
@@ -0,0 +1,4 @@
+<?php
+session_name('dessa_template');
+session_start();
+
---
+++ pages/dashboard/main.php
@@ -0,0 +1,117 @@
+<style>
+    .page-header {
+        font-weight: 900;
+    }
+
+    .info-box {
+        background-color: #00296b;
+    }
+
+    .info-card {
+        background-color: #00296b;
+        color: white;
+        border: 2px solid white;
+        height: 130px;
+    }
+
+    .info-icon {
+        background-color: #fdc500;
+        color: #00296b;
+    }
+
+    table tr{
+        text-align: center;
+    }
+</style>
+
+<div class="content-header">
+    <div class="container-fluid">
+        <div class="card">
+            <!--Card-->
+            <div class="card-header">
+                <h3 class="card-title">Summary</h3>
+            </div>
+            <div class="card-body">
+                <div class="row text-center">
+                    <!--Card 1-->
+                    <div class="col-md-4 col-sm-12" style="height: auto;">
+                        <div class="info-box info-card">
+                            <span class="info-box-icon info-icon">
+                                <i class="fas fa-users"></i>
+                            </span>
+                            <div class="info-box-content">
+                                <span class="info-box-text" style="font-size: 30px;">Total Accounts</span>
+                                <span class="info-box-number info-number" style="font-size: 33px;">1,234</span>
+                            </div>
+                        </div>
+                    </div>
+                    <!--Card 2-->
+                    <div class="col-md-4 col-sm-12" style="height: auto;">
+                        <div class="info-box info-card">
+                            <span class="info-box-icon info-icon">
+                                <i class="fas fa-chart-line"></i>
+                            </span>
+                            <div class="info-box-content">
+                                <span class="info-box-text" style="font-size: 30px;">Admin</span>
+                                <span class="info-box-number" style="font-size: 33px;">4567</span>
+                            </div>
+                        </div>
+                    </div>
+                    <!--Card 3-->
+                    <div class="col-md-4 col-sm-12" style="height: auto;">
+                        <div class="info-box info-card">
+                            <span class="info-box-icon info-icon">
+                                <i class="fas fa-database"></i>
+                            </span>
+                            <div class="info-box-content">
+                                <span class="info-box-text" style="font-size: 30px;">User</span>
+                                <span class="info-box-number" style="font-size: 33px;">8912</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!--Table-->
+        <div class="row">
+            <!--Table 1-->
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Table Summary</h3>
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table id="categoriesTable" class="table table-bordered table-hover" style="width:100%">
+                                <thead>
+                                    <tr>
+                                        <th class="id-column">ID</th>
+                                        <th class="defect-column">Employee #</th>
+                                        <th class="name-column">Name</th>
+                                        <th class="actions-column">Username</th>
+                                        <th class="analysis-column">Password</th>
+                                        <th class="analysis-column">Department</th>
+                                        <th class="analysis-column">Role</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div><!-- /.container-fluid -->
+</div>
